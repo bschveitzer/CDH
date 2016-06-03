c02c916dd1d973e2f82551ce8a16f918ff7582fc
Bernardo <bess1101@hotmail.com>
2016-06-03 18:34:53 -0300

    Finalização de interface e criação do botão relatorio

--- public/scripts/controllers/homeController.js
+++ public/scripts/controllers/homeController.js
@@ -2,6 +2,22 @@
  * Created by Osvaldo on 23/09/15.
  */
 
-app.controller("homeController",['$scope', function ($scope) {
+app.controller("homeController",['$scope','$location', 'utilvalues', function ($scope,$location, utilvalues) {
+
+    $scope.classes = utilvalues.rotaatual;
+
+    $scope.trocaRota=function (local) {
+        limpanav(local, function () {
+            utilvalues.rotaatual[local] = 'active';
+            $location.path('/'+local);
+        });
+    };
+
+    var limpanav = function (local, cb) {
+        for(var id in $scope.classes){
+            utilvalues.rotaatual[id] = '';
+        }
+        cb();
+    };
 
 }]);
--- public/scripts/controllers/relatorioController.js
+++ public/scripts/controllers/relatorioController.js
@@ -1,7 +1,22 @@
 /**
  * Created by labtic on 01/06/2016.
  */
+app.controller("relatorioController",['$scope','$location', 'utilvalues', function ($scope,$location, utilvalues) {
 
-app.controller("relatorioController",['$scope', function ($scope) {
+    $scope.classes = utilvalues.rotaatual;
+
+    $scope.trocaRota=function (local) {
+        limpanav(local, function () {
+            utilvalues.rotaatual[local] = 'active';
+            $location.path('/'+local);
+        });
+    };
+
+    var limpanav = function (local, cb) {
+        for(var id in $scope.classes){
+            utilvalues.rotaatual[id] = '';
+        }
+        cb();
+    };
 
 }]);
